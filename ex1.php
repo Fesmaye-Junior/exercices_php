8ffc59a08ab6a1506cc27d9bc86bad2844525fa6
Fesmaye-Junior Mohamed <mohamed-fesmaye-junior@lycee-jeanrostand.fr>
2023-10-03 17:04:56 +0200

    moyenne

--- ex1.php
+++
@@ -1,5 +0,0 @@
-<?php
-include ('statistique.php');
-$note=array($note_maths=15 , $note_francais=12 , $note_histoire_geo=9 );
-echo moyenne($note) ;
-?>
--- triangle.php
+++ triangle.php
@@ -1,1 +1,14 @@
 <?php
+function triangle ($nb_lignes)
+{
+
+    for ($i=0 ; $i<$nb_lignes; $i++){
+        for ($j=0; $j<$nb_colonne; $j++){
+            echo $nb_lignes="*";
+        }
+        $nb_lignes=3;
+        $nb_colonne=5;
+       return ($nb_lignes;$nb_colonne);
+}
+}
+?>
